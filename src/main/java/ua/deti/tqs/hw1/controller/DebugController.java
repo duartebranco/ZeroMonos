0ece532e32483167c64321ff4c7d27d95487999f
duartebranco <duartebranco35@gmail.com>
2025-11-02 14:28:47 +0000

    import booking

--- src/main/java/ua/deti/tqs/hw1/controller/DebugController.java
+++ src/main/java/ua/deti/tqs/hw1/controller/DebugController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ua.deti.tqs.hw1.model.Booking;
 import ua.deti.tqs.hw1.service.BookingService;
 import ua.deti.tqs.hw1.service.MunicipalityService;
 
